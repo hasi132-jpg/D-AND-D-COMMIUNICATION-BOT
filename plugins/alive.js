e48e3c4b4e5c1080f6c0f9620e42e48ffe7e56f4
hasitha prasad <prasadhasitha80@gmail.com>
2025-08-07 02:34:51 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,7 +3,7 @@ const {cmd , commands} = require('../command')
 
 cmd({
     pattern: "alive",
-    alias:["ok","හරි"."bot"],
+    alias:["ok","yes"."bot"],
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
